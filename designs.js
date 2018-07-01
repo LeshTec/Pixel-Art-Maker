dd2af0a06624b0d5a49db0f5a9040a4319ee81ee
LeshTec <37936832+LeshTec@users.noreply.github.com>
2018-07-01 23:26:22 +0300

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,35 +1,36 @@
 // Select color input
 // Select size input
-var height, width, color;
+var height,width,color;
 
 // When size is submitted by the user, call makeGrid()
-$(documnet).ready(function() {
-$('#sizePicker').submit(function (event) {
-    event.preventDefault();
-    height = $('#inputHeight').val();
-    width = $('#inputWeight').val();
-    makeGrid(height, width);
+
+$('#sizePicker').submit(function (event){
+	event.preventDefault();
+	height= $('#inputHeight').val();
+	width= $('#inputWidth').val();
+	makeGrid(height,width);
+
+    //console.log('Height: ' + height + ' and width: ' + width);
+
 })
 
 function makeGrid(x, y) {
+	$('tr').remove();
+	for (var i = 1; i<=x; i++){
+		$('#pixelCanvas').append('<tr id=table' + i + '></tr>');
+		for (var j =1; j<=y; j++){
+			$('#table' + i).append('<td></td>');
+		}
+	}
 
-  // Your code goes here!
-  for (var i = 1; i <= x; i++) {
-      $('#pixelCanvas').append('<tr id=table' + i + '></tr>');
-      for (var j = 1; j <=y; j++) {
-          $('#table' + i).append('<td></td>');
-        }
-    }
-
-    // add colors to the cells
     $('td').click(function addColor(){
-        color = $('#colorPicker').val();
-
-        if ($(this).attr('style')) {
-            $(this).removeAttr('style')
-        } else {
-            $(this).attr('style', 'background-color:' + color);
-        }
-    })
-
+		color = $('#colorPicker').val();
+		if ($(this).attr('style')){
+			$(this).removeAttr('style')
+		}else {
+			$(this).attr('style','background-color:' + color);
+		}
+	})
 }
+
+
